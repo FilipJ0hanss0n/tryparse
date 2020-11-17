43101c8b783c5662ceaea7d265b5dd0f9fea3884
alvin <alja02003@utb.vaxjo.se>
2020-11-17 10:35:43 +0100

    filer

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryparse2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultat;
+            if (!int.TryParse(Console.ReadLine(), out resultat))
+                Console.WriteLine("Skriv en siffra");
+        }
+        public static bool TryParse(string s, out int result)
+        {
+            try
+            {
+                result = int.Parse(s);
+                return true;
+            }
+            catch
+            {
+                result = 0;
+                return false;
+            }
+        }
+    }
+}
